b062dfecd805fe04abeca71b5e1050f8387be2d5
Dylan Turnbull <dylanturn@gmail.com>
2021-04-22 01:43:20 +0000

    oops, forgot these

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+module.exports = Object.freeze({
+    DEPOSIT_ENERGY: 'deposit_energy',
+    WITHDRAW_ENERGY: 'withdraw_energy',
+    CREEP_ROLES: {
+        HARVESTER: "🌽",
+        BUILDER: "🔨",
+        TRANSPORTER: "🚚",
+        WORKER: "🔧"
+    }
+});
---
+++ src/creep.role.transporter.js
@@ -0,0 +1,25 @@
+const constants = require('./constants')
+const creep_util = require('./creep.util')
+
+function getSpec(){
+	let role_spec = creep_util.GetBaseRoleSpec()
+    role_spec.primary_role = constants.CREEP_ROLES.TRANSPORTER
+    role_spec.valid_secondary_roles = [constants.CREEP_ROLES.WORKER]
+	role_spec.parts = [ WORK, CARRY, MOVE ]
+    role_spec.active_role = role_spec.primary_role
+    return role_spec
+}
+
+function run(creep){
+	creep.say(creep.memory.active_role, false)
+	return creep
+}
+
+module.exports = {
+    GetSpec(){
+        return getSpec()
+    },
+    Run(creep){
+        return run(creep)
+    }
+};
